fda0e96add50ea8fe7b5498ac60fe2eaa25648fd
Saurabh <saurabhxcodes@gmail.com>
2026-07-07 00:45:30 +0530

    Complete version 1.0 of predictionSimulator-V1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("script loaded");
+// required fixed datas;
 let colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"];
 let sizes = ["big", "small"];
 
@@ -15,19 +15,49 @@ const bigSmallBtnsEl = document.querySelectorAll(".big-small-btns");
 const currentBetsContainerEl = document.getElementById("currentBetContainer");
 
 
+
 // lower container DOMs;
 const lowerContainerEl = document.getElementById("lowerContainer");
-const historyTabsEl = document.querySelectorAll(".result-myBets-button");
+const historyTabsBtnEl = document.querySelectorAll(".result-myBets-button");
 const resultInfoContainerEl = document.getElementById("resultInfoContainer");
 const mybetsContainerEl = document.getElementById("myBetsInfoContainer");
 const betPlacingContainerEl = document.getElementById("placeBetContainer");
 const amountInputEl = document.getElementById("coinsInput");
 const placeBetBtnEl = document.getElementById("placeBetBtn");
 
+
+resultInfoContainerEl.classList.add("d-block");
+mybetsContainerEl.classList.add("d-none");
+
+
+historyTabsBtnEl.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+        let choice = event.target.dataset.choice;
+        if (choice === "gameResults") {
+            resultInfoContainerEl.classList.remove("d-none");
+            resultInfoContainerEl.classList.add("d-block");
+
+            mybetsContainerEl.classList.remove("d-block");
+            mybetsContainerEl.classList.add("d-none");
+
+        }
+        else {
+            mybetsContainerEl.classList.remove("d-none");
+            mybetsContainerEl.classList.add("d-block");
+
+            resultInfoContainerEl.classList.remove("d-block");
+            resultInfoContainerEl.classList.add("d-none");
+        }
+    });
+});
+
+
 let betContainerHidden = true;
-//....
-let gameHistory = [];
-let myBets = [];
+//....storing datas;
+let gameHistory = JSON.parse(localStorage.getItem("gameHistory")) || [];
+let myBets = JSON.parse(localStorage.getItem("myBets")) || [];
+
+
 function getUserDetails() {
     let parsedUserDetails = JSON.parse(localStorage.getItem("userDetails"));
     if (parsedUserDetails === null) {
@@ -78,8 +108,119 @@ function displayUserDetails() {
     balanceEl.textContent = userDetails.balance;
 }
 
-function compareBetWithResult() {
+function disableBetBtns() {
+    colorBtnsEl.forEach(function (button) {
+        button.disabled = true;
+    });
+    bigSmallBtnsEl.forEach(function (button) {
+        button.disabled = true;
+    });
+}
+function enableBetBtns() {
+    colorBtnsEl.forEach(function (button) {
+        button.disabled = false;
+    });
+    bigSmallBtnsEl.forEach(function (button) {
+        button.disabled = false;
+    });
+}
 
+let currentBet = getCurrentBet();
+if (currentBet.roundNo !== null) {
+    displayCurrentBetContainer(currentBet);
+}
+
+
+function createAndAppendMyBethistory(myBetObject) {
+    let historycontainerEl = document.createElement("div");
+    historycontainerEl.classList.add("d-flex", "flex-row", "justify-content-between", "mt-3");
+    mybetsContainerEl.prepend(historycontainerEl);
+    let roundNoEl = document.createElement("p");
+    roundNoEl.textContent = "Round: " + myBetObject.roundNo;
+    historycontainerEl.appendChild(roundNoEl);
+
+    if (myBetObject.type === "color") {
+        let colorEl = document.createElement("div");
+        colorEl.classList.add("result-color-circles");
+        colorEl.style.backgroundColor = myBetObject.prediction;
+        historycontainerEl.appendChild(colorEl);
+    }
+    else if (myBetObject.type === "size") {
+        let sizeResultEl = document.createElement("p");
+        sizeResultEl.classList.add("size-result");
+        sizeResultEl.textContent = myBetObject.prediction;
+        historycontainerEl.appendChild(sizeResultEl);
+    }
+
+    if (myBetObject.status === "win") {
+        let winEl = document.createElement("p");
+        winEl.textContent = "+" + myBetObject.amount * 1.8;
+        winEl.style.color = "green";
+        historycontainerEl.appendChild(winEl);
+    }
+    else if (myBetObject.status === "lose") {
+        let loseEl = document.createElement("p");
+        loseEl.textContent = "-" + myBetObject.amount;
+        loseEl.style.color = "red";
+        historycontainerEl.appendChild(loseEl);
+    }
+
+    if (mybetsContainerEl.children.length > 20) {
+        mybetsContainerEl.lastChild.remove();
+    }
+}
+function saveMyBetHistory(myBetObject) {
+    myBets.unshift(myBetObject);
+
+    if (myBets.length > 20) {
+        myBets.pop();
+    }
+
+    localStorage.setItem("myBets", JSON.stringify(myBets));
+}
+
+function compareBetWithResult(result) {
+    let status;
+    if (currentBet.type === "color") {
+        if (currentBet.prediction === result.color) {
+            userDetails.balance += currentBet.amount * 1.8;
+            saveUserDetails();
+            displayUserDetails();
+            status = "win";
+        }
+        else {
+            status = "lose";
+        }
+
+    }
+    else if (currentBet.type === "size") {
+        if (currentBet.prediction === result.size) {
+            userDetails.balance += currentBet.amount * 1.8;
+            saveUserDetails();
+            displayUserDetails();
+            status = "win";
+        }
+        else {
+            status = "lose";
+        }
+    }
+    let myBetObject = {
+        roundNo: currentBet.roundNo,
+        type: currentBet.type,
+        prediction: currentBet.prediction,
+        amount: currentBet.amount,
+        status: status
+    };
+    saveMyBetHistory(myBetObject);
+    createAndAppendMyBethistory(myBetObject);
+
+    currentBet = {
+        roundNo: null,
+        type: null,
+        prediction: null,
+        amount: null
+    };
+    saveCurrentBet(currentBet);
 }
 
 function createAndAppendHistory(result) {
@@ -127,6 +268,11 @@ function handelRound() {
     gameState.roundNo++;
     gameState.roundStartTimestamp = Date.now();
     saveGameState();
+    enableBetBtns();
+    hideCurrentBetContainer();
+    if (currentBet.roundNo != null) {
+        compareBetWithResult(result);
+    }
 }
 
 function displayGameState() {
@@ -157,7 +303,7 @@ function calculateTimeRemaining() {
     let roundStartTime = gameState.roundStartTimestamp;
     let elapsedSeconds = Math.floor((currentTime - roundStartTime) / 1000);
 
-    let remainingSeconds = 30 - elapsedSeconds;
+    let remainingSeconds = 20 - elapsedSeconds;
     return remainingSeconds;
 }
 
@@ -173,14 +319,59 @@ function showBetContainer(prediction) {
     placeBetBtnEl.textContent = prediction;
     betContainerHidden = false;
 }
-function addCurrentBet(prediction) {
+
+function displayCurrentBetContainer(currentBet) {
+    currentBetsContainerEl.classList.add("d-none", "d-md-block");
+    let headingEl = document.createElement("h1");
+    headingEl.textContent = "Your Bet";
+    headingEl.classList.add("current-bet-heading");
+    currentBetsContainerEl.appendChild(headingEl);
+
+    let roundNoEl = document.createElement("p");
+    roundNoEl.textContent = "Round No: " + currentBet.roundNo;
+    roundNoEl.classList.add("current-bet-round");
+    currentBetsContainerEl.appendChild(roundNoEl);
+
+    let choiceEl = document.createElement("p");
+    choiceEl.textContent = "Choice: " + currentBet.prediction;
+    choiceEl.classList.add("current-bet-round");
+    currentBetsContainerEl.appendChild(choiceEl);
+
+    let amountEl = document.createElement("p");
+    amountEl.textContent = "Amount: " + currentBet.amount;
+    amountEl.classList.add("current-bet-round");
+    currentBetsContainerEl.appendChild(amountEl);
+}
+
+function hideCurrentBetContainer() {
+    currentBetsContainerEl.innerHTML = "";
+    currentBetsContainerEl.classList.remove("d-none", "d-md-block");
+    currentBetsContainerEl.classList.add("d-none", "d-md-none");
+
+}
+
+function saveCurrentBet() {
+    localStorage.setItem("currentBet", JSON.stringify(currentBet));
+}
+
+function getCurrentBet() {
+    return JSON.parse(localStorage.getItem("currentBet")) || {
+        roundNo: null,
+        type: null,
+        prediction: null,
+        amount: null
+    };
+}
+
+function addCurrentBet(prediction, type) {
     let betAmount = parseInt(amountInputEl.value);
     if (Number.isNaN(betAmount) || betAmount <= 0 || betAmount > userDetails.balance) {
-        return;
+        alert("Enter a valid amount");
     }
     else {
         currentBet.roundNo = gameState.roundNo,
-            currentBet.prediction = prediction,
+            currentBet.type = type;
+        currentBet.prediction = prediction,
             currentBet.amount = betAmount;
         userDetails.balance = userDetails.balance - betAmount;
         saveUserDetails();
@@ -188,24 +379,22 @@ function addCurrentBet(prediction) {
         betPlacingContainerEl.style.display = "none";
         amountInputEl.value = "";
         selectedPrediction = null;
-        console.log(currentBet);
     }
+
+    saveCurrentBet(currentBet);
+    disableBetBtns();
+    displayCurrentBetContainer(currentBet);
 }
 
-let currentBet = {
-    roundNo: null,
-    prediction: null,
-    amount: null
-
-};
 
 let selectedPrediction = null;
+let selectedType = null;
 colorBtnsEl.forEach(function (button) {
     button.addEventListener("click", function (event) {
         selectedPrediction = event.target.dataset.color;
+        selectedType = "color";
         let remainingSeconds = calculateTimeRemaining();
         if (remainingSeconds > 5) {
-            addCurrentBet(selectedPrediction);
             showBetContainer(selectedPrediction);
         }
     });
@@ -213,9 +402,9 @@ colorBtnsEl.forEach(function (button) {
 bigSmallBtnsEl.forEach(function (button) {
     button.addEventListener("click", function (event) {
         selectedPrediction = event.target.dataset.choice;
+        selectedType = "size";
         let remainingSeconds = calculateTimeRemaining();
         if (remainingSeconds > 5) {
-            addCurrentBet(selectedPrediction);
             showBetContainer(selectedPrediction);
         }
     });
@@ -234,7 +423,6 @@ function generateResult() {
     return newResult;
 }
 
-let result = generateResult();
 
 
 // makin UI update every second;
@@ -243,5 +431,15 @@ let inervalId = setInterval(displayGameState, 1000);
 
 // placing bets;
 placeBetBtnEl.addEventListener("click", function () {
-    addCurrentBet(selectedPrediction);
+    addCurrentBet(selectedPrediction, selectedType);
 });
+
+// persisting gameHistory on refresh;
+for (let i = gameHistory.length - 1; i >= 0; i--) {
+    createAndAppendHistory(gameHistory[i]);
+}
+
+// persisting myHistory on refresh;
+for (let i = myBets.length - 1; i >= 0; i--) {
+    createAndAppendMyBethistory(myBets[i]);
+}
